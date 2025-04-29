19fc1afd2606cd24a664fc35b4bf29b09903fb65
Mr.spaidy <72148615+Mrspaidy@users.noreply.github.com>
2025-04-29 15:24:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const ytdl = require('ytdl-core');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+app.get('/download', async (req, res) => {
+  const videoURL = req.query.url;
+  if (!ytdl.validateURL(videoURL)) {
+    return res.status(400).send('Invalid YouTube URL');
+  }
+  
+  const info = await ytdl.getInfo(videoURL);
+  res.header('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp4"`);
+
+  ytdl(videoURL, { format: 'mp4' }).pipe(res);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
